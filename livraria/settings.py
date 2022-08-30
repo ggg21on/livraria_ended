de1a94f3edcd24fe623e5824c5f3182c61db1ede
Gabriel Gomes <gomesgalikosky@gmail.com>
2022-08-30 14:12:40 -0300

    fix

--- livraria/settings.py
+++ livraria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mmhr)b47lsiizzptw@2p!li1s6&6a#h1-y2x()mxx^q=$pbc)i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
