49fca1481ab939f476fb7c63499da6114c86182f
ggg21on <gomesgalikosky@gmail.com>
2022-10-17 08:33:08 -0300

    livraria

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
 
-from core.models import Autor, Categoria, Editora, Livro
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core.models import Autor, Categoria, Editora, Livro, Usuario
 
 admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Livro)
+
+class UsuarioAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "cpf", "telefone", "data_nascimento")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+admin.site.register(Usuario, UsuarioAdmin)
